95ca096ba22a0799948c4e85e875d4085b9e73b3
catborn <itnoz@yandex.ru>
2024-09-07 01:18:52 +0300

    add batman

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,6 +5,12 @@ function Hero(props) {
       <img src={props.imgLink} alt={props.name} width="500px"></img>
       <div>
         <h2>{props.name}</h2>
+        <p>{props.universe}</p>
+        <p>{props.alterego}</p>
+        <p>{props.occupation}</p>
+        <p>{props.friends}</p>
+        <p>{props.superpowers}</p>
+        <p>{props.info}</p>
         <hr />
       </div>
     </div>
